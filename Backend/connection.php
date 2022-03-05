6bc3ab529975d807836d244ccda64d52ae0115f3
Sanket Chougule <sanketchaugule407@gmail.com>
2022-03-05 13:04:33 +0530

    backend

--- Backend/connection.php
+++ Backend/connection.php
@@ -1,12 +1,26 @@
 <?php
-$host ="localhost";
-$user = "root";  
-$password = '';  
-$db_name = "bscit";  
-  
-$con = mysqli_connect($host, $user, $password, $db_name);  
-if(mysqli_connect_errno()) {  
-    die("Failed to connect with MySQL: ". mysqli_connect_error());  
-}  
+    $host = "localhost";
+	$user="root";
+	$password="";
+	$dbName="mydatabase";
+	
+	$conn =mysqli_connect($host,$user,$password,$dbName);
+	
+	$username =$_POST["username"];
+	$class = $_POST["class_name"];
+	$roll = $_POST["roll_no"];
+	if ($conn){
+	echo "connected";
+	}
+	else {
+	 die("Eror : " .mysqli_connect_error());
+	}
 
+	$sql="INSERT INTO `player_info`(`username`, `Class_name`, `roll_no`) VALUES ('$username','$class',$roll)";
+	
+	$result=mysqli_query($conn,$sql);
+	if($result){
+	echo "inserted";}
+	else {
+	echo "not inserted";}
 ?>
